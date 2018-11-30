fb1cde96623487de093beb1f27535f20e3adfdad
nbari <nbari@tequila.io>
2018-11-30 13:28:52 +0100

    sync Fri Nov 30 13:28:52 CET 2018

--- galera/start.sh
+++ galera/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+docker-compose down
+find . -type d -name "db" -execdir sh -c 'rm -rf {}/*' \;
 docker-compose up node0 &
 
 while ! test "$(mysql -h 127.0.0.1 --port 43306 -u root --password=test -ANe"show global status like 'wsrep_cluster_size'" 2>/dev/null| awk '{print $2}')" == 1; do sleep 3; done
